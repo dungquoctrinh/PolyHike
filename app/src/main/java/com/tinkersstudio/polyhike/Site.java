6d3e8d1e058e668583d55468f5ef98263df56724
Roshan Prabhakar <roshancvp@gmail.com>
2017-01-14 10:46:32 -0800

    Site stuff

--- app/src/main/java/com/tinkersstudio/polyhike/Site.java
+++ app/src/main/java/com/tinkersstudio/polyhike/Site.java
@@ -4,6 +4,28 @@ package com.tinkersstudio.polyhike;
  * Created by Owner on 1/14/2017.
  */
 
-public class Site {
+public class Site implements {
+
+    protected int parkingSpots;
+    protected int currentHikers;
+    protected String description;
+    protected int popularity;
+    protected int trailLength;
+    protected int difficulty;
+
+    public Site(int parkingSpots, int currentHikers, String description, int popularity, int trailLength, int difficulty) {
+        this.parkingSpots = parkingSpots;
+        this.currentHikers = currentHikers;
+        this.description = description;
+        this.popularity = popularity;
+        this.trailLength = trailLength;
+        this.difficulty = difficulty;
+    }
+
     
+
+
+
+
+
 }
