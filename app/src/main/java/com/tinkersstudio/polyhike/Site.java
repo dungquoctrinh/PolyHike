0572c823bc2837c7a89f647d2368daf8c1554587
Roshan Prabhakar <roshancvp@gmail.com>
2017-01-14 11:02:06 -0800

    Added comparator

--- app/src/main/java/com/tinkersstudio/polyhike/Site.java
+++ app/src/main/java/com/tinkersstudio/polyhike/Site.java
@@ -4,25 +4,30 @@ package com.tinkersstudio.polyhike;
  * Created by Owner on 1/14/2017.
  */
 
-public class Site implements {
+public class Site {
 
     protected int parkingSpots;
     protected int currentHikers;
     protected String description;
     protected int popularity;
     protected int trailLength;
-    protected int difficulty;
+    protected float difficulty;
 
-    public Site(int parkingSpots, int currentHikers, String description, int popularity, int trailLength, int difficulty) {
+    public Site(int parkingSpots, int currentHikers, String description, int popularity, int trailLength) {
         this.parkingSpots = parkingSpots;
         this.currentHikers = currentHikers;
         this.description = description;
         this.popularity = popularity;
         this.trailLength = trailLength;
-        this.difficulty = difficulty;
+
+        //Set difficulty
+        this.difficulty = trailLength + currentHikers + popularity - parkingSpots;
+
     }
 
-    
+
+
+
 
 
 
